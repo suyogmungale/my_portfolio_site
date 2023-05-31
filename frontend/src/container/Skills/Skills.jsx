9e813d3f726df88d6c97c9e44715644563064b36
suyogmungale <suyogmungale5@gmail.com>
2023-06-01 00:13:33 +0530

    final

--- frontend/src/container/Skills/Skills.jsx
+++ frontend/src/container/Skills/Skills.jsx
@@ -1,10 +1,65 @@
 import React from 'react'
-import './Skills.sass'
+import './Skills.scss'
+import { useState, useEffect } from 'react'
+import { Tooltip } from 'react-tooltip'
+import { animate, motion } from 'framer-motion'
+import { AppWrap } from '../../wrapper'
+import { urlFor, client } from '../../client'
 
 const Skills = () => {
+  const [experiences, setExperiences] = useState([]);
+  const [skills, setSkills] = useState([]);
+
+  useEffect(() => {
+    const query = '*[_type == "experiences"]';
+    const skillsQuery = '*[_type == "skills"]';
+
+    client.fetch(query).then((data) => {
+      console.log(data)
+      setExperiences(data);
+    });
+
+    client.fetch(skillsQuery).then((data) => {
+      setSkills(data);
+    });
+  }, []);
+
+
+
   return (
-    <div>Skills</div>
+    <>
+      <h2 className='head-text'>Skills <span>and</span> Experiance</h2>
+      <div className="app__skills-container">
+        <motion.div
+          className='app__skills-list'>
+          {skills?.map((skill) => (
+
+            <motion.div
+              whileInView={{ opacity: [0, 1] }}
+              transition={{ duration: 0.5 }}
+              className="app__skills-item app__flex"
+              key={skill?.name}
+            >
+              <div
+                className="app__flex"
+                style={{ backgroundColor: skill.bgColor }}
+              >
+                <img src={urlFor(skill?.icon)} alt={skill?.name} />
+              </div>
+              <p className="p-text">{skill?.name}</p>
+
+            </motion.div>
+
+          ))}
+        </motion.div>
+
+
+
+
+
+      </div>
+    </>
   )
 }
 
-export default Skills
+export default AppWrap(Skills, 'skills')
--- frontend/src/container/index.js
+++ frontend/src/container/index.js
@@ -4,6 +4,7 @@ import Header from './Header/Header';
 import Skills from './Skills/Skills';
 import Testimonial from './Testimonial/Testimonial';
 import Work from './Work/Work';
+import Certificates from './certificates/Certificates';
 
 export {
   About,
@@ -12,4 +13,5 @@ export {
   Skills,
   Testimonial,
   Work,
+  Certificates
 };
